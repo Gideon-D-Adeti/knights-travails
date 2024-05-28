206d13207313ac2602d198425cde8298dd631144
Gideon-D-Adeti <gideondelaliadeti@gmail.com>
2024-05-28 20:56:02 +0000

    Add knightMoves function with it's directions array

--- knightsTravails.js
+++ knightsTravails.js
@@ -9,3 +9,16 @@ class Queue {
     return this.elements.shift();
   }
 }
+
+function knightMoves(start, end) {
+  const directions = [
+    [2, 1],
+    [2, -1],
+    [-2, 1],
+    [-2, -1],
+    [1, 2],
+    [1, -2],
+    [-1, 2],
+    [-1, -2],
+  ];
+}
